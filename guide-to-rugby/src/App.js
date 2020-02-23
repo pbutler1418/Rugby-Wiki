0f3c7a3bc5fb0fe27ad725636636f9ef09b31ca9
Pierce Butler <Pierce@Melissas-MacBook-Pro.local>
2020-02-23 18:36:37 -0500

    Created ReadMe and tested API

---
+++ guide-to-rugby/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import BASE_URL from "./components/constants"
+
+function App() {
+  console.log(BASE_URL)
+  return (
+    <div className="App">
+      <h1>Rugby</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ guide-to-rugby/src/components/constants.js
@@ -0,0 +1,4 @@
+const API_KEY = "4013017"
+export const BASE_URL = `https://www.thesportsdb.com/api/v1/json/${API_KEY}/eventshighlights.php?l=Six_Nations_Championship`
+
+export default BASE_URL
